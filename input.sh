07c8e100018cf324896ce01800d78dfce83a744e
d-devop <divya5guntaka@gmail.com>
2023-01-23 19:56:49 +0530

    sample

---
+++ input.sh
@@ -0,0 +1,7 @@
+#read -p 'Enter MySQL Root Username:' user
+#read -s -p 'Enter MySQL Root Password:' pass
+
+#echo "Username & Password from read = $user & $pass"
+
+echo Username = $1
+echo Password = $2
